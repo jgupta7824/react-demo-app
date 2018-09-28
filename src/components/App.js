f1b7e7dcea3f2f7bd8c265ad78d17a36cc1c8414
vishwanath <vishwackh89@gmail.com>
2018-09-28 09:48:36 +0530

    28/09/2018 jquery poper added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-// import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -9,6 +6,11 @@ import Sidenav from './Sidenav';
 
 import '../assets/stylesheets/base.css';
 
+//  Third Party libraries 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../lib/Jquery';
+import '../lib/Popper';
+import 'bootstrap/dist/js/bootstrap.min';
 
 const App = ({ children }) => {
   return (
